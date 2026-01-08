f323fc13dc6f9237ce388bb690138b576aaeb43b
ajii999 <rosalesjilon@gmail.com>
2026-01-09 02:37:09 +0800

    Fix styling on Railway: Switch to CDN links for Bootstrap and Font Awesome
    - Replace local asset() calls with CDN links in admin, tenant, and staff layouts
    - Fixes broken styling on Railway deployment
    - Ensures CSS/JS load correctly regardless of asset path resolution

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Admin Dashboard - Boarding House Management')</title>
     
-    <!-- Bootstrap 5 CSS - Local file for offline use -->
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <!-- Bootstrap 5 CSS - Using CDN for Railway compatibility -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     
-    <!-- Font Awesome CSS - Local file for offline use -->
-    <link href="{{ asset('css/font-awesome/all.min.css') }}" rel="stylesheet">
+    <!-- Font Awesome CSS - Using CDN for Railway compatibility -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer">
     
     @stack('styles')
     
@@ -662,8 +662,8 @@
         </div>
     </div>
     
-    <!-- Bootstrap 5 JS Bundle - Local file for offline use -->
-    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <!-- Bootstrap 5 JS Bundle - Using CDN for Railway compatibility -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     
     @stack('scripts')
     
--- resources/views/layouts/tenant.blade.php
+++ resources/views/layouts/tenant.blade.php
@@ -6,15 +6,11 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Tenant Portal - Boarding House Management')</title>
     
-    <!-- Bootstrap 5 CSS - Local file for offline use -->
-    <!-- Download from: https://getbootstrap.com/docs/5.3/getting-started/download/ -->
-    <!-- Place bootstrap.min.css in public/css/ -->
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <!-- Bootstrap 5 CSS - Using CDN for Railway compatibility -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     
-    <!-- Font Awesome CSS - Local file for offline use -->
-    <!-- Download from: https://fontawesome.com/download -->
-    <!-- Place font-awesome folder in public/css/ -->
-    <link href="{{ asset('css/font-awesome/all.min.css') }}" rel="stylesheet">
+    <!-- Font Awesome CSS - Using CDN for Railway compatibility -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer">
     
     @stack('styles')
     
@@ -749,8 +745,8 @@
     </div>
     
     <!-- Bootstrap 5 JS Bundle - Local file for offline use -->
-    <!-- Place bootstrap.bundle.min.js in public/js/ -->
-    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <!-- Bootstrap 5 JS Bundle - Using CDN for Railway compatibility -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     
     @stack('scripts')
     
